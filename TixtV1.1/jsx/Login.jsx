181ff98cc1a7c8a1bc9f22db26f90e7a240abffc
Unknown <victor.loredo46@gmail.com>
2018-05-29 15:53:51 +0200

    InitDB

--- TixtV1.1/jsx/Login.jsx
+++ TixtV1.1/jsx/Login.jsx
@@ -1,28 +1,61 @@
 const React = require('react');
 
 class Login extends React.Component {
- 	render() {
- 		return (
- 			<div className="form-group">
-	 			<div className="input-group col-md-6">
-	 				<input type="text"
-	 					className="form-control" 
-	 					ref={(ref) => this.textInput = ref} 
-	 					placeholder="Email" />
-                         <input type="text"
-	 					className="form-control" 
-	 					ref={(ref) => this.textInput = ref} 
-	 					placeholder="Mot de passe" />
-	                
-	                <span className="input-group-btn">
-	        			<button className="btn btn-primary" type="button">
-	        				<span className="glyphicon glyphicon-search"></span>
-	        			</button>
-	      			</span>
-	 			</div>
-	 		</div>
- 		)
-  	}
+
+    constructor() {
+        super();
+        this.writeUser = this.writeUser.bind(this);
+        this.keyDownHandler = this.keyDownHandler.bind(this);
+    }
+
+    //Méthode appelée dès que le composant est chargé 
+    //dans le navigateur
+    componentDidMount() {
+        this.mdpInput.focus();
+        this.emailInput.focus();
+    }
+
+    writeUser() {
+
+        return fetch('/barbero')
+        .then((response) => {
+          console.log("dshjfnqdsjkfnsqdljkfnqdsjf");
+        })
+        .catch((error) => {
+          console.error(error);
+        });
+    }
+
+    keyDownHandler(event) {
+        //On lance la recherche si l'utilisateur 
+        //saisit la touche Enter
+        if (event.keyCode == 13) {
+            this.writeUser();
+        }
+    }
+
+    render() {
+        return (
+            <div className="form-group">
+                <div className="input-group col-md-6">
+                    <input type="text"
+                        className="form-control"
+                        ref={(ref) => this.emailInput = ref}
+                        placeholder="Email" />
+                    <input type="text"
+                        className="form-control"
+                        ref={(ref) => this.mdpInput = ref}
+                        placeholder="Mot de passe" />
+
+                    <span className="input-group-btn">
+                        <button onClick={this.writeUser} className="btn btn-primary" type="button">
+                            <span className="glyphicon glyphicon-search"></span>
+                        </button>
+                    </span>
+                </div>
+            </div>
+        )
+    }
 }
 
 module.exports = Login;
--- TixtV1.1/server.js
+++ TixtV1.1/server.js
@@ -11,6 +11,33 @@ const data = require('./data/products.js');
 
 const app = express();
 
+const mysql = require('mysql');
+const bodyParser = require('body-parser');
+
+var config = require('config.json')('configs.json');
+
+const connection = mysql.createConnection({
+  host: config.mysql.host,
+  user: config.mysql.user,
+  password: config.mysql.password,
+  database: config.mysql.database,
+});
+
+connection.connect();
+
+global.db = connection;
+
+var sql = "SELECT * FROM `utilisateur`";
+
+app.get('/barbero', function (req, res) {
+  db.query(sql, function (err, results) {
+    console.log(results);
+    res.send("COUCOU");
+
+  });
+});
+
+
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
@@ -18,25 +45,27 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(require('connect-livereload')());
 
 app.get('/', function (req, res) {
-  const products = {'products' : data};
+  const products = {
+    'products': data
+  };
 
-	const htmlProductListComponent = ReactDOMServer.renderToString( 
+  const htmlProductListComponent = ReactDOMServer.renderToString(
     ProductListComponent(products)
   );
 
   res.render("index", {
-  	component: htmlProductListComponent,
-  	context: JSON.stringify(products)
+    component: htmlProductListComponent,
+    context: JSON.stringify(products)
   });
 
 });
 
 app.get('/products', function (req, res) {
-	res.setHeader('Content-Type', 'application/json');
-	
+  res.setHeader('Content-Type', 'application/json');
+
   const productName = req.query.productName.toLowerCase();
-  const productContainingNameParam = 
-      _.filter(data, (p) => p.name.toLowerCase().indexOf(productName) > -1);
+  const productContainingNameParam =
+    _.filter(data, (p) => p.name.toLowerCase().indexOf(productName) > -1);
 
   res.send(JSON.stringify(productContainingNameParam));
 });
