3c18b76c97cd8feb3a06654368383ec04907299f
Kévin Rodrigues <kevin.rodrigues@ynov.com>
2018-06-05 12:09:57 +0200

    ajout select véhicules

--- TixtV1.1/jsx/Vehicules.jsx
+++ TixtV1.1/jsx/Vehicules.jsx
@@ -4,21 +4,11 @@ class Vehicules extends React.Component {
     render() {
       return (
         <div className="vehicules">
-          <h1>SÉLECTIONNEZ PARMI UNE CENTAINE DE VÉHICULES</h1>
+          <h1>SÉLECTIONNEZ PARMI PLUSIEURS VÉHICULES</h1>
           <div className="vignettes">
-              <div className="picto"></div>
-              <div className="picto"></div>
-              <div className="picto"></div>
-          </div>
-          <div className="vignettes">
-              <div className="picto"></div>
-              <div className="picto"></div>
-              <div className="picto"></div>
-          </div>
-          <div className="vignettes">
-              <div className="picto"></div>
-              <div className="picto"></div>
-              <div className="picto"></div>
+              <div className="picto left">RENAULT Clio</div>
+              <div className="picto middle">QUANTINO</div>
+              <div className="picto right">RENAULT Twingo</div>
           </div>
         </div>
       );
