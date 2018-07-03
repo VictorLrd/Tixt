7fe20089d06d8a3944466dada7591bb53b779492
Kévin Rodrigues <kevin.rodrigues@ynov.com>
2018-07-03 09:47:46 +0200

    Modif dev react

--- TixtV1.1/jsx/Vehicules.jsx
+++ TixtV1.1/jsx/Vehicules.jsx
@@ -5,16 +5,18 @@ class Vehicules extends React.Component {
       return (
         <div className="vehicules">
           <h1>SÉLECTIONNEZ PARMI UNE CENTAINE DE VÉHICULES</h1>
-          <div className="vignettes">
-              <div className="picto left">RENAULT Clio</div> 
-              <div className="picto middle">QUANTINO</div> 
-              <div className="picto right">RENAULT Twingo</div> 
-          </div>
-          <div className="s-vignettes"> 
-              <div className="picto left">FIAT Tipo</div> 
-              <div className="picto middle">PEUGEOT 208</div> 
-              <div className="picto right">HYUNDAI i10</div> 
-          </div> 
+            <div className="wrapper-vignettes">
+              <div className="vignettes">
+                  <div className="picto left">RENAULT Clio</div> 
+                  <div className="picto middle">QUANTINO</div> 
+                  <div className="picto right">RENAULT Twingo</div> 
+              </div>
+              <div className="s-vignettes"> 
+                  <div className="picto left">FIAT Tipo</div> 
+                  <div className="picto middle">PEUGEOT 208</div> 
+                  <div className="picto right">HYUNDAI i10</div> 
+              </div> 
+            </div> 
         </div>
       );
     }
