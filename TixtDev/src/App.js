5603a8559c5fb4497402e614bb846b721ce8e522
Kévin Rodrigues <kevin.rodrigues@ynov.com>
2018-07-03 17:19:47 +0200

    modif

--- TixtDev/src/App.js
+++ TixtDev/src/App.js
@@ -1,15 +1,12 @@
 import React, { Component } from 'react';
-import cors from 'cors';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import { HomeLayout } from './layouts.js'
-import { Inscription } from './layouts.js'
-
 import './App.css'
 import AddProductsLayout from '../client/layouts/AddProductsLayout/index.js';
-
+ 
 export default class App extends Component {
-
-
+ 
+ 
   render() {
     return (
       <BrowserRouter>
@@ -18,16 +15,11 @@ export default class App extends Component {
             <Route path="/home">
                 <HomeLayout />
             </Route>
-<<<<<<< Updated upstream
-            <Route path="/inscription">
-                <Inscription />
-=======
             <Route path="/ajout-vehicules">
                 <AddProductsLayout/>
             </Route>
             <Route>
                 <HomeLayout />
->>>>>>> Stashed changes
             </Route>
           </Switch>
         </div>
